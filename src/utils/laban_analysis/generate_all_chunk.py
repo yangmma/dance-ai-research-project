95bb87594b1ff40ad4aab7f52d26de23a2a15a96
J.Z. Zhou <flamemaikuai@gmail.com>
2024-10-01 19:27:50 +1300

    add new

---
+++ src/utils/laban_analysis/generate_all_chunk.py
@@ -0,0 +1,46 @@
+import generate_features
+from tqdm import tqdm
+import polars as pl
+import os
+
+
+def main(input_dir: str):
+    fnames = os.listdir(input_dir)
+    df = pl.DataFrame()
+    out_path = "./generated_complete"
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+
+    chunk_size = 1000
+    for i, name in enumerate(tqdm(fnames)):
+        if name.split("-")[2][0] != "3":
+            continue
+        elif name.split("-")[3][0] != "0":
+            continue
+
+        full_path = os.path.join(input_dir, name)
+        feat_df = generate_features.main(full_path, True)
+        df = df.vstack(feat_df)
+
+        if i % chunk_size == 0 and i != 0:
+            full_out_path = os.path.join(out_path, f"./generated_features_complete_{i // chunk_size}.parquet")
+            df = df.with_columns(
+                name_orig = pl.col("name").str.split(by=".").list.get(0),
+                music = pl.col("name").str.split(by="-").list.get(1).str.split(".").list.get(0),
+                win = pl.col("name").str.split(by="-").list.get(2).str.split("").list.get(0),
+                num = pl.col("name").str.split(by="-").list.get(3).str.split("").list.get(0),
+                win_c = pl.col("name").str.split(by="-").list.get(4).str.split("").list.get(0),
+            )
+            df = df.drop(
+                pl.col("name"),
+            ).with_columns(
+                name = pl.col("name_orig"),
+            )
+            df.write_parquet(full_out_path)
+            df = pl.DataFrame()
+    
+    full_out_path = os.path.join(out_path, f"./generated_features_complete_{(i // chunk_size) + 1}.parquet")
+    df.write_parquet(full_out_path)
+
+if __name__ == "__main__":
+    main("./post_processed_out_split_win")
